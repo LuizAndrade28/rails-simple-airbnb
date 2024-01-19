e94dcc3782acb20fafb352767f967eff57ea59bf
LuizAndrade <lcfsandrade@gmail.com>
2024-01-19 19:47:26 -0300

    Challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+17.times do
+  Flat.create!(
+    name: "Light & Spacious Garden Flat London #{Faker::Address.country}",
+    address: Faker::Address.street_address,
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: rand(30..120),
+    number_of_guests: rand(1..5)
+  )
+end
+
